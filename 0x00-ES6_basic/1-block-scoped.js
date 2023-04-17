e9a7c28223472ab35182975f9278d26d26220c94
iakev <kirimikmwongera@gmil.com>
2023-04-17 17:25:35 +0000

    modified to use let so as to take advantage of hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    var task = false;
-    var task2 = true;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
