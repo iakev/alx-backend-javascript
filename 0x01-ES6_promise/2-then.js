f60aa3a27bd2645ca640806203b8e28360a00681
iakev <kirimikmwongera@gmil.com>
2023-04-18 16:05:52 +0000

    added a script that consumes a promise when it resolves, rejects and finally

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    const ret = {
+      status: 200,
+      body: 'success',
+    };
+    return ret;
+  })
+    .catch(() => {
+      Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
