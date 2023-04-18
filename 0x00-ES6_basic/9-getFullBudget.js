d91794d11606f154bae6c325a805980decfed61f
iakev <kirimikmwongera@gmil.com>
2023-04-18 07:41:06 +0000

    modified method properties to arrow syntax

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,14 +1,14 @@
 import getBudgetObject from './7-getBudgetObject.js';
 
-export default function getFullBudgetObject(income, gdp, capita) {
+export default function getFullBudgetObject (income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: function (income) {
-      return `$${this.income}`;
+    getIncomeInDollars: (income) => {
+      return `$${income}`;
     },
-    getIncomeInEuros: function (income) {
-      return `${this.income} euros`;
+    getIncomeInEuros: (income) => {
+      return `${income} euros`;
     },
   };
 
