985d1a6fd9912e8ee260c2ebb4498120209c2834
iakev <kirimikmwongera@gmil.com>
2023-04-19 19:52:08 +0000

    modified to properly format error message

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,7 +3,8 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (e) {
-    queue.push(e);
+    
+    queue.push(`${e.name}: ${e.message}`);
   }
   queue.push('Guardrail was processed');
   return queue;
