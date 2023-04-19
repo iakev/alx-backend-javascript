97094d2d7fc701d7b520b49f39e4ee79cbd3d856
iakev <kirimikmwongera@gmil.com>
2023-04-19 19:06:07 +0000

    modified for linter

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,8 +1,6 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   const promises = [chinaDownload, USDownload];
   const returnPromise = Promise.any(promises);
-  const result = returnPromise.then((res) => {
-    return res;
-  });
+  const result = returnPromise.then((res) => res);
   return result;
 }
