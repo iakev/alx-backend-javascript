5f57ad147b62bbbb770340d3f58aff2d2945b023
iakev <kirimikmwongera@gmil.com>
2023-04-20 17:43:06 +0000

    added a script that creates an abstract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (!(this.evacuationWarningMessage in this)) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    if (typeof (sqft) !== 'number') {
+      throw new TypeError('Sqft should be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof (value) !== 'number') {
+      throw new TypeError('Sqft should be a number');
+    }
+    this._sqft = value;
+  }
+}
