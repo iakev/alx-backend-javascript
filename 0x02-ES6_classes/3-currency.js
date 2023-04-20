efb782f2d4e7058f3ed9d429e2c7901607222329
iakev <kirimikmwongera@gmil.com>
2023-04-20 13:57:11 +0000

    modified to add type checks on costructor

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,5 +1,11 @@
 export default class Currency {
   constructor(code, name) {
+    if (typeof (code) !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof (name) !== 'string') {
+      throw new TypeError('name must be a string');
+    }
     this._code = code;
     this._name = name;
   }
