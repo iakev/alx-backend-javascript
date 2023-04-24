bebc9f08c014b1000d1ecbabdd12d18c6107e0d9
iakev <kirimikmwongera@gmil.com>
2023-04-24 09:31:41 +0000

    modified to export an array as a default export without a variable name

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,3 +1,5 @@
+export const listOfStudents = [];
+
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -36,10 +38,10 @@ export class StudentHolberton {
   }
 }
 
-export const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-export const student2 = new StudentHolberton('John', 'Doe', class2020);
-export const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-export const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-export const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+export default [student1, student2, student3, student4, student5];
