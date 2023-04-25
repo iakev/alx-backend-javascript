8c10e2935e9573ead7e2f056889f24927aa1dd40
iakev <kirimikmwongera@gmil.com>
2023-04-25 13:24:52 +0000

    modified function to change slice end index according to the length of the startString parameter

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
   const resultStrArr = [];
   for (const item of set) {
-    if (item.slice(0, 3) === startString) {
-      resultStrArr.push(`${item.slice(3)}`);
+    if (item.slice(0, startString.length) === startString) {
+      resultStrArr.push(`${item.slice(startString.length)}`);
     }
   }
   const resultStr = resultStrArr.join('-');
