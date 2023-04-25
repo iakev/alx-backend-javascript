29fa8ee9151438570d5132349328e0bd1544c9c4
iakev <kirimikmwongera@gmil.com>
2023-04-25 13:26:57 +0000

    added an edge case when the length of the startString is abnormal

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (startString.length == 0) {
+    return '';
+  }
   const resultStrArr = [];
   for (const item of set) {
     if (item.slice(0, startString.length) === startString) {
