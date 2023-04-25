fccd5f2689f78e45e81537f2e11ffb6ec04957ac
iakev <kirimikmwongera@gmil.com>
2023-04-25 15:00:07 +0000

    added a function that updates a map passed as an argument accordingly and returns the updated map or throws an error if can't update

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(map) {
+  for (const [key, value] of map) {
+    if (value === 1) {
+      try {
+        map.set(key, 100);
+      } catch (error) {
+        throw new Error('Cannot process');
+      }
+    }
+  }
+}
