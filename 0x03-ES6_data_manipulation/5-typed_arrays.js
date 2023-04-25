266daad677fa2829add1323bb5d1e2ac3a883d63
iakev <kirimikmwongera@gmil.com>
2023-04-25 12:24:33 +0000

    modified to return a DataView of an ArrayBuffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
 
-  const int8View = new Int8Array(buffer);
-
+  const view = new DataView(buffer);
   try {
-    int8View[position] = value;
+    view.setInt8(position, value);
   } catch (error) {
-    throw new RangeError('Position outside range');
+    throw new RangeError('Postion outside range');
   }
-  return int8View;
+
+  return view;
 }
