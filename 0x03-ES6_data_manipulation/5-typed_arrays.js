fde0abe468477d057180f25f0bff203abafe832b
iakev <kirimikmwongera@gmil.com>
2023-04-25 12:12:28 +0000

    added a script that creates an ArrayBuffer populates it accordingly and returns it

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+
+  const int8View = new Int8Array(buffer);
+
+  try {
+    int8View[position] = value;
+  } catch (error) {
+    throw new RangeError('Position outside range');
+  }
+  return buffer;
+}
