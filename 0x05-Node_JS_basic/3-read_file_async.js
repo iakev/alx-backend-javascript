f5c3a0929ae7f89c8437d690c3ee781419200251
iakev <kirimikmwongera@gmail.com>
2023-06-13 06:34:03 +0000

    Added a script with a function reading a file asychronously and returning a Promise

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n');
+        const fieldSet = new Set();
+        let studentsCnt = 0;
+        for (let index = 0; index < lines.length; index += 1) {
+          const line = lines[index];
+          const values = line.split(',');
+          if (values.length === 4) {
+            if (values[values.length - 1] !== 'field') {
+              studentsCnt += 1;
+              fieldSet.add(values[values.length - 1]);
+            }
+          }
+        }
+        const firstNames = {};
+        fieldSet.forEach((value) => {
+          firstNames[value] = [];
+        });
+
+        for (let index = 0; index < lines.length; index += 1) {
+          const line = lines[index];
+          const values = line.split(',');
+          if (values.length === 4) {
+            if (Object.prototype.hasOwnProperty.call(firstNames, values[values.length - 1])) {
+              firstNames[values[values.length - 1]].push(values[0]);
+            }
+          }
+        }
+        console.log(`Number of students: ${studentsCnt}`);
+        for (const [key, value] of Object.entries(firstNames)) {
+          console.log(`Number of students in ${key} ${value.length}. List: ${value.join(', ')}`);
+        }
+        resolve(data);
+      }
+    });
+  });
+};
