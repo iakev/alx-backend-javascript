7919ba0c424aba8cc616b1e1f8d7175a808d1181
iakev <kirimikmwongera@gmail.com>
2023-06-15 05:43:25 +0000

    Added a test for the function rounding and summing numbers

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,26 @@
+const calculateNumber = require('./0-calcul');
+const assert = require('assert');
+
+describe('calculateNumber', function() {
+  it('should round arguments and return the sum of them', function () {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('should round one argument and return the sum of them', function () {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it('should round both arguments correctly and return the sum of them', function () {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should round both arguments correctly and return the sum of them', function () {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it('should round negative arguments and return the sum of them', function () {
+    assert.equal(calculateNumber(-2.5, 5.6), 4);
+  });
+  it('should round zero-valued arguments and return the sum of them', function () {
+    assert.equal(calculateNumber(-0.5, 5.6), 6);
+  });
+  it('should round zero arguments and return the sum of them', function () {
+    assert.equal(calculateNumber(0, 1), 1);
+  });
+});
