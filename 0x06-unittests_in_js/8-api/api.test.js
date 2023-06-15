f26b0e69af32d8bee352315f3aa72e2bbeba5cac
iakev <kirimikmwongera@gmail.com>
2023-06-15 18:37:23 +0000

    Added a express http server and an integration test suite that test the functionality of it

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const request = require('request');
+
+describe('Index page testing', function() {
+  it('should return the correct status code for a succesful get', function(done) {
+    request('http://localhost:7865', function(error, response, body) {
+      chai.expect(response.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+  it('should return the correct result', function(done) {
+    request('http://localhost:7865', function(error, response, body) {
+      chai.expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+  it('should return correct error code on failure', function(done) {
+    request('http://localhost:7865/any', function(error, response, body) {
+      chai.expect(response.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});
