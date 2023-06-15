b6452dfc01ce636ebd1b49a85438ce82bec0b849
iakev <kirimikmwongera@gmail.com>
2023-06-15 15:56:00 +0000

    Added a test for an asynchronous code

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const chai = require('chai');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('tests an asynchronous piece of code', function(done) {
+    getPaymentTokenFromAPI(true)
+      .then(function(result) {
+        chai.expect(result.data).to.equal('Successful response from the API');
+      });
+    done();
+  });
+});
