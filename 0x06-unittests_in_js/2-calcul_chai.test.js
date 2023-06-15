64357240b4b8a63e637f78442f578386fec9ff81
iakev <kirimikmwongera@gmail.com>
2023-06-15 08:25:08 +0000

    Added a test for the function in 2-calcul.js

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,80 @@
+const calculateNumber = require('./1-calcul');
+const chai = require('chai');
+
+describe('calculateNumber', function() {
+  describe('#SUM', function() {
+    it('should round arguments and return the sum of them', function () {
+      chai.expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+    });
+    it('should round one argument and return the sum of them', function () {
+      chai.expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+    });
+    it('should round both arguments correctly and return the sum of them', function () {
+      chai.expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+    });
+    it('should round both arguments correctly and return the sum of them', function () {
+      chai.expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
+    });
+    it('should round negative arguments and return the sum of them', function () {
+      chai.expect(calculateNumber('SUM', -2.5, 5.6)).to.equal(4);
+    });
+    it('should round zero-valued negative arguments and return the sum of them', function () {
+      chai.expect(calculateNumber('SUM', -0.5, -5.6)).to.equal(-6);
+    });
+    it('should round zero arguments and return the sum of them', function () {
+      chai.expect(calculateNumber('SUM', 0, 0)).to.equal(0);
+    });
+  });
+  describe('#SUBTRACT', function() {
+    it('should round arguments and return the difference', function () {
+      chai.expect(calculateNumber('SUBTRACT', 14, 3)).to.equal(11);
+    });
+    it('should round arguments and return the correct difference', function () {
+      chai.expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
+    });
+    it('should round negative arguments and return the correct difference', function () {
+      chai.expect(calculateNumber('SUBTRACT', -1, -3)).to.equal(2);
+    });
+    it('should round decimal arguments and return the correct difference', function () {
+      chai.expect(calculateNumber('SUBTRACT', 14.3, 3.7)).to.equal(10);
+    });
+    it('should round one decimal arguments and return the correct difference', function () {
+      chai.expect(calculateNumber('SUBTRACT', 1.5, 3)).to.equal(-1);
+    });
+    it('should round arguments and return the correct difference', function () {
+      chai.expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
+    });
+    it('should round negative arguments and return the correct difference', function () {
+      chai.expect(calculateNumber('SUBTRACT', -10.3, -30.6)).to.equal(21);
+    });
+    it('should round zero valued arguments and return the correct difference', function () {
+      chai.expect(calculateNumber('SUBTRACT', 0, 0)).to.equal(0);
+    });
+  });
+  describe('#DIVIDE', function(){
+    it('should round arguments and return the quotient', function () {
+      chai.expect(calculateNumber('DIVIDE', 24, 3)).to.equal(8);
+    });
+    it('should round decimal arguments and return the quotient', function () {
+      chai.expect(calculateNumber('DIVIDE', 23.7, 2.8)).to.equal(8);
+    });
+    it('should round arguments and return Error for divide by 0', function () {
+      chai.expect(calculateNumber('DIVIDE', 24, 0)).to.equal('Error');
+    });
+    it('should round zero dividied arguments', function () {
+      chai.expect(calculateNumber('DIVIDE', 0.1, 3)).to.equal(0);
+    });
+    it('should round negative arguments and return the quotient', function () {
+      chai.expect(calculateNumber('DIVIDE', -24, -3)).to.equal(8);
+    });
+    it('should round arguments and return the correct quotient', function () {
+      chai.expect(calculateNumber('DIVIDE', -23.7, 2.8)).to.equal(-8);
+    });
+    it('should round arguments and return the correct quotient', function () {
+      chai.expect(calculateNumber('DIVIDE', -23.7, -2.8)).to.equal(8);
+    });
+    it('should round all zero arguments and return the correct quotient', function () {
+      chai.expect(calculateNumber('DIVIDE', 0, 0)).to.equal('Error');
+    });
+  });
+});
