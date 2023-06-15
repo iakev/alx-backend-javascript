50cba401d6271cbc996bdeb5c215d9658a0c3bf5
iakev <kirimikmwongera@gmail.com>
2023-06-15 14:37:46 +0000

    Added a test that test console logs, using before and after hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function (){
+  let calculateNumber = {};
+  let print = {};
+  beforeEach(function() {
+    print = sinon.spy(console, 'log');
+  });
+  this.afterEach(function() {
+    print.restore();
+  })
+  it('validates Utils function called once', function() {
+    sendPaymentRequestToApi(100, 20);
+    chai.expect(print.calledWith('The total is: 120')).to.be.true;
+    chai.expect(print.calledOnce).to.be.true;
+  });
+  it('validates arguments called in Utils function', function(){
+    sendPaymentRequestToApi(10, 10);
+    chai.expect(print.calledWith('The total is: 20')).to.be.true;
+    chai.expect(print.calledOnce).to.be.true;
+  });
+});
