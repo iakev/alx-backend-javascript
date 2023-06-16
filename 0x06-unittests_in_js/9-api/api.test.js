230a44223cf932a849a945bf4719b9883be41596
iakev <kirimikmwongera@gmail.com>
2023-06-16 08:19:21 +0000

    Modified to add test for the index page

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -20,4 +20,22 @@ describe('Cart page', function() {
       done();
     });
   });
+  it('should return the correct status code for a succesful get', function(done) {
+    request('http://localhost:7865', function(error, response, body) {
+      chai.expect(response.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+  it('should return the correct result', function(done) {
+    request('http://localhost:7865', function(error, response, body) {
+      chai.expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+  it('should return correct error code on failure', function(done) {
+    request('http://localhost:7865/any', function(error, response, body) {
+      chai.expect(response.statusCode).to.be.equal(404);
+      done();
+    });
+  });
 });
